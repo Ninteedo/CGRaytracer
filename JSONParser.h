f2a9195db4315cb44aa704e2bac996698712d6ef
Richard Holmes <richard.gordon.holmes@gmail.com>
2023-11-02 16:58:45 +0000

    Add JSONParser
    - https://chat.openai.com/c/bbc1b2cb-9d39-411c-bd30-15854fd88f75
    - ChatGPT output was broken because it consumed chars at the switch statement in parserValue

---
+++ JSONParser.h
@@ -0,0 +1,86 @@
+#ifndef JSON_PARSER_H
+#define JSON_PARSER_H
+
+#include <string>
+#include <map>
+#include <vector>
+
+class JsonParseError : public std::exception {
+public:
+    JsonParseError(const std::string& json, size_t position, const std::string& message);
+
+    virtual const char* what() const noexcept override;
+
+private:
+    std::string errorMessage;
+};
+
+class JsonValue;
+
+typedef std::map<std::string, JsonValue> JsonObject;
+typedef std::vector<JsonValue> JsonArray;
+
+class JsonValue {
+public:
+    enum Type {
+        Null,
+        Boolean,
+        Number,
+        String,
+        Object,
+        Array
+    };
+
+    JsonValue();
+    JsonValue(bool value);
+    JsonValue(int value);
+    JsonValue(double value);
+    JsonValue(const std::string& value);
+    JsonValue(const char* value);
+    JsonValue(const JsonObject& value);
+    JsonValue(const JsonArray& value);
+
+    Type getType() const;
+    bool isNull() const;
+    bool asBool() const;
+    int asInt() const;
+    double asDouble() const;
+    const std::string& asString() const;
+    const JsonObject& asObject() const;
+    const JsonArray& asArray() const;
+
+private:
+    Type type;
+    union {
+        bool boolValue;
+        double numberValue;
+    };
+    std::string stringValue;
+    JsonObject objectValue;
+    JsonArray arrayValue;
+};
+
+class JsonParser {
+public:
+    JsonParser();
+    JsonValue parse(const std::string& json);
+
+private:
+    size_t pos;
+    std::string json;
+
+    void skipWhitespace();
+    char nextChar();
+    char peekChar();
+    bool match(const std::string& pattern);
+
+    JsonValue parseValue();
+    JsonObject parseObject();
+    JsonArray parseArray();
+    std::string parseString();
+    double parseNumber();
+    bool parseBoolean();
+    void parseNull();
+};
+
+#endif // JSON_PARSER_H
---
+++ test/json_parser_test.cpp
@@ -0,0 +1,99 @@
+#include "../JSONParser.h"
+#include <iostream>
+#include <cassert>
+
+void testSimpleValues() {
+    JsonParser parser;
+    JsonValue value;
+
+    // Test null
+    value = parser.parse("null");
+    assert(value.isNull());
+
+    // Test boolean
+    value = parser.parse("true");
+    assert(value.asBool() == true);
+
+    value = parser.parse("false");
+    assert(value.asBool() == false);
+
+    // Test numbers
+    value = parser.parse("42");
+    assert(value.asInt() == 42);
+
+    value = parser.parse("-42");
+    assert(value.asInt() == -42);
+
+    value = parser.parse("3.14");
+    assert(value.asDouble() == 3.14);
+
+    // Test strings
+    value = parser.parse("\"Hello World\"");
+    assert(value.asString() == "Hello World");
+}
+
+void testObjectsAndArrays() {
+    JsonParser parser;
+    JsonValue value;
+
+    // Test object
+    value = parser.parse(R"({"name":"John", "age":30})");
+    assert(value.asObject().at("name").asString() == "John");
+    assert(value.asObject().at("age").asInt() == 30);
+
+    // Test array
+    value = parser.parse("[1, 2, 3, 4]");
+    assert(value.asArray().size() == 4);
+    assert(value.asArray()[0].asInt() == 1);
+    assert(value.asArray()[1].asInt() == 2);
+    assert(value.asArray()[2].asInt() == 3);
+    assert(value.asArray()[3].asInt() == 4);
+}
+
+void testNestedStructures() {
+    JsonParser parser;
+    JsonValue value;
+
+    // Test nested structures
+    value = parser.parse(R"({"person":{"name":"John", "age":30}, "numbers":[1, 2, 3]})");
+    assert(value.asObject().at("person").asObject().at("name").asString() == "John");
+    assert(value.asObject().at("person").asObject().at("age").asInt() == 30);
+    assert(value.asObject().at("numbers").asArray()[0].asInt() == 1);
+    assert(value.asObject().at("numbers").asArray()[1].asInt() == 2);
+    assert(value.asObject().at("numbers").asArray()[2].asInt() == 3);
+}
+
+void testErrorConditions() {
+    JsonParser parser;
+    bool caught;
+
+    // Test malformed JSON
+    caught = false;
+    try {
+        parser.parse("{invalid_json}");
+    }
+    catch (const std::exception&) {
+        caught = true;
+    }
+    assert(caught);
+
+    // Test incomplete JSON
+    caught = false;
+    try {
+        parser.parse("{\"name\":");
+    }
+    catch (const std::exception&) {
+        caught = true;
+    }
+    assert(caught);
+}
+
+int main() {
+    testSimpleValues();
+    testObjectsAndArrays();
+    testNestedStructures();
+    testErrorConditions();
+
+    std::cout << "All tests passed!" << std::endl;
+    return 0;
+}
